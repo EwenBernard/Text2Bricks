5e8a30b338eaec9e65338d395cd5166b84f419c7
EwenBernard <ewen.bernard@efrei.net>
2024-12-17 18:41:16 +0100

    update graph data

--- text2brick/models/GraphWorldData.py
+++ text2brick/models/GraphWorldData.py
@@ -1,15 +1,24 @@
 from pydantic import BaseModel, Field
 from typing import List, Tuple
-from networkx import Graph
+import networkx as nx
+import torch_geometric
+from torch_geometric.utils.convert import from_networkx
+import numpy as np
 
 
 class GraphLegoWorldData(BaseModel):
-    graph : Graph = Field(..., description="Graph representation of the Lego world")
+    graph : nx.Graph = Field(..., description="Graph representation of the Lego world")
     brick_dim : Tuple[int, int, int] = (2, 1, 2)
-    mapping_table : List[List[List[int]]] = Field(default_factory=list, description="Mapping table of the Lego world")
+    world_dim : Tuple[int, int, int] = (10, 10, 1)
+    
+    def graph_to_torch(self) -> torch_geometric.data.Data:
+        "converts the graph to torch_geometric.data.Data"
+        return from_networkx(self.graph)
+    
+    def graph_to_np(self) -> np.ndarray:
+        "converts the graph to numpy array"
+        return nx.to_numpy_array(self.graph)
 
-    def __str__(self) -> str:
-        return f"Piece Count : {len(self.world)}\n" + "\n".join([brick.coords() for brick in self.world])
 
-    def str_full_infos(self) -> str:
-        return "Lego World Bricks\n" + "\n".join([str(brick) for brick in self.world])
+    
+    
