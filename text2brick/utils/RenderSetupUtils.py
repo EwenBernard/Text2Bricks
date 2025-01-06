5adf35976aaad9d408450c1f7e0f925bded6ae70
Gaël <gael.gourdin@ensea.fr>
2025-01-06 18:08:12 +0100

    Add ltron setup functions

---
+++ text2brick/utils/RenderSetupUtils.py
@@ -0,0 +1,8 @@
+
+def install_ltron_asset():
+    from ltron.scripts.ltron_asset_installer import main
+    main()
+
+def install_splendor_asset():
+    from splendor.scripts.splendor_asset_installer import main
+    main()
